6271a7abd4cee59af6a6fb05e1b5cb25a3cf7c72
shixiang <shixiangcool@163.com>
2018-07-29 21:25:09 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Header/>
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+*{margin: 0; padding: 0; box-sizing: border-box;}
+*::after{box-sizing: border-box;}
+*::before{box-sizing: border-box;}
+li{list-style: none;}
+a{text-decoration: none; color: #C7C3C7;}
+
+</style>
