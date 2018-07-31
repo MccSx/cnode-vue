288b1490550ba6c430d35e05d4f3157eb177da77
shixiang <shixiangcool@163.com>
2018-08-01 00:05:20 +0800

    添加列表组件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <Header/>
+    <PostList/>
     <!-- <router-view/> -->
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import PostList from './components/PostList'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    PostList
   }
 }
 </script>
@@ -20,6 +23,8 @@ export default {
 *{margin: 0; padding: 0; box-sizing: border-box;}
 *::after{box-sizing: border-box;}
 *::before{box-sizing: border-box;}
+body{background: #E1E1E1;}
+h1,h2,h3,h4,h5,h6{font-weight: normal;}
 li{list-style: none;}
 a{text-decoration: none; color: #C7C3C7;}
 
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,19 +1,61 @@
 <template>
-  <div class="">
-
+  <div class="posts">
+    <div class="loading" v-if="isLoading">
+      <img src="../assets/loading.gif" alt="">
+    </div>
+    <div class="post-list">
+      <ul>
+        <li v-for="post in posts" :key="post.id">
+          <img :src="post.author.avatar_url" alt="">
+          <span class="count">
+            <span class="reply-count">{{post.reply_count}}</span>/<span class="visit-count">{{post.visit_count}}</span>
+          </span>
+          <div class="message">
+            <p>{{post.title}}</p>
+            <span>{{post.last_reply_at}}</span>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name : '',
+  name : 'PostList',
   data() {
     return {
-
+      isLoading: true,
+      posts: []
     }
   },
+  methods: {
+    getData() {
+      this.$axios.get('https://cnodejs.org/api/v1/topics', {
+        params: {
+          limit: 20,
+          page: 1
+        }
+      })
+        .then((res) => {
+          this.posts = res.data.data
+          this.isLoading = false
+        })
+        .catch((err) => {console.log(err)})
+    }
+  },
+  beforeMount() {
+    this.getData()
+  }
 }
 </script>
 
 <style scoped>
+.posts{width: 75%; margin: 0 auto; background: #FFFFFF;}
+.post-list ul li{height: 50px; border-bottom: 1px solid #F0F0F0; display: flex; flex-direction: row; align-items: center;}
+.post-list ul li img{height: 65%; border-radius: 3px; margin-left: 1%;}
+.post-list ul li .count{display: flex; justify-content: center; align-items: center; width: 5%;}
+.post-list ul li .reply-count{font-size: 14px;}
+.post-list ul li .visit-count{font-size: 12px; color: #B8B8B8;}
+.post-list ul li .message{width: 100%; display: flex; flex-direction: row; justify-content: space-between;}
 </style>
